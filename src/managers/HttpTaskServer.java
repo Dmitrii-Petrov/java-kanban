434e2601a09fe05b007720c78127035a4ecb012e
Dmitrii Petrov <BanananaBatman@yandex.ru>
2023-01-26 17:26:35 +0300

    .

--- src/managers/HttpTaskServer.java
+++ src/managers/HttpTaskServer.java
@@ -1,4 +1,19 @@
 package managers;
 
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class HttpTaskServer {
+    HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
+    TaskManager fileBackedTasksManager = Managers.getFileBackedTaskManager(Path.of("./resources/1.csv"));
+
+
+    public HttpTaskServer() throws IOException {
+    }
 }
--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -1,10 +1,15 @@
 package managers;
 
+import java.nio.file.Path;
+
 public class Managers {
 
 
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
+    }
+    public static TaskManager getFileBackedTaskManager(Path path) {
+        return new FileBackedTasksManager(path);
     }
 
     public static HistoryManager getDefaultHistory() {
